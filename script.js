9acc18fe533e68074cd463df8873e634cc62a5e2
Arrikrishnan01 <krishari11052001@gmail.com>
2023-01-28 05:06:58 -0800

    file added

---
+++ script.js
@@ -0,0 +1,56 @@
+var set_time=document.querySelector("h1");
+setTimeout(()=>{
+    set_time.innerText='10'
+    document.body.style.color='#FF6347';
+    document.body.style.backgroundColor='#f2d5a5';    
+        setTimeout(()=>{
+            set_time.innerText='9'
+            document.body.style.color='#694833';
+            document.body.style.backgroundColor='#BFD833';
+            setTimeout(()=>{
+                set_time.innerText='8'
+                document.body.style.color='#f2d5a5.';
+                document.body.style.backgroundColor='# EC9787';
+                setTimeout(()=>{
+                    set_time.innerText='7'
+                    document.body.style.color='#FF6347';
+                    document.body.style.backgroundColor=' #E6E6FA';
+                    setTimeout(()=>{
+                        set_time.innerText='6'
+                        document.body.style.color='#E32227';
+                        document.body.style.backgroundColor=' #6EA1D4';
+                        setTimeout(()=>{
+                            set_time.innerText='5'
+                            document.body.style.color='black';
+                            document.body.style.backgroundColor='red';
+                            setTimeout(()=>{
+                                set_time.innerText='4'
+                                document.body.style.color='green';
+                                document.body.style.backgroundColor='white';
+                                setTimeout(()=>{
+                                    set_time.innerText='3'
+                                    document.body.style.color='#E32227';
+                                    document.body.style.backgroundColor=' #eb9687';
+                                    setTimeout(()=>{
+                                        set_time.innerText='2'
+                                        document.body.style.color='black';
+                                        document.body.style.backgroundColor='white';
+                                        setTimeout(()=>{
+                                            set_time.innerText='1'
+                                            document.body.style.color='yellow';
+                                            document.body.style.backgroundColor='blue';
+                                            setTimeout(()=>{
+                                                set_time.innerText='Happy Independence Day'
+                                                document.body.style.color='#262b29';
+                                                document.body.style.backgroundColor='#00ffff';
+                                            },1000);
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+},1000);
